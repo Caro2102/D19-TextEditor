3ddd91a51520fc95128296b33a10a5316c3fc3af
Carolina Cruz <caro21_cruz@hotmail.com>
2023-02-16 01:48:20 -0600

    Se agrego logica para instalar la PWA

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,26 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Lógica para instalar la PWA
-// TODO: Agregar un controlador de eventos al evento `beforeinstallprompt`
-window.addEventListener('beforeinstallprompt', (event) => {});
+// Controlador de eventos al evento `beforeinstallprompt`
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
-// TODO: Implementar un controlador de eventos de clic en el elemento `butInstall`
-butInstall.addEventListener('click', async () => {});
+// Controlador de eventos de clic en el elemento `butInstall`
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return;
+    }
+    promptEvent.prompt();
+    
+    window.deferredPrompt = null;
+    
+    butInstall.classList.toggle('hidden', true);
+});
 
-// TODO: Agregar un controlador para el evento `appinstalled`
-window.addEventListener('appinstalled', (event) => {});
+// Controlador para el evento `appinstalled`
+window.addEventListener('appinstalled', (event) => {
+      window.deferredPrompt = null;
+});
